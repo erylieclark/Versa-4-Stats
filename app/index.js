57503ce11f615c265a3e72d078cfa666a10c3d15
erylieclark <40705885+erylieclark@users.noreply.github.com>
2023-01-06 23:19:38 -0800

    happy with visual aspect of progress circles

--- app/index.js
+++ app/index.js
@@ -1,23 +1,58 @@
 import * as document from "document";
 import * as messaging from "messaging";
 import * as simpleClock from "./features/clock";
-import * as progressData from "./features/progress_circles";
+import * as activityArcs from "./features/activity";
 
 let background = document.getElementById("background");
+
+// Elements to change theme color
 let txtTime = document.getElementById("txtTime");
 let txtDate = document.getElementById("txtDate");
 let gradientDividerTop = document.getElementById("gradientDividerTop");
 let gradientDividerBottom = document.getElementById("gradientDividerBottom");
+let circle1 = document.getElementById("circle1");
+let circle2 = document.getElementById("circle2");
+let circle3 = document.getElementById("circle3");
+//let circle_elements = document.getElementsByClassName("arc-style");
+
+// Elements to change activity
+let circles = ["circle1", "circle2", "circle3"]
+// Default Values
+// Presumably I can export this list as a global variable?
+let activities = ["steps", "distance", "calories", "activeMinutes"]
+
 
 // Message is received
 messaging.peerSocket.onmessage = evt => {
   console.log(`App received: ${JSON.stringify(evt)}`);
+
+  // Change the theme color of all elements
   if (evt.data.key === "themeColor" && evt.data.newValue) {
+    // Get the Color
     let color = JSON.parse(evt.data.newValue);
-    console.log(`Setting date and time color: ${color}`);
+    console.log(`Setting Theme Color: ${color}`);
+
+    // Date and Gradient Bars
     txtDate.style.fill = color;
     gradientDividerTop.gradient.colors.c2 = color;
     gradientDividerBottom.gradient.colors.c2 = color;
+
+    // Progress Circles
+    circle1.getElementById("arcFront").style.fill = color;
+    circle2.getElementById("arcFront").style.fill = color;
+    circle3.getElementById("arcFront").style.fill = color;
+  }
+  
+  // If evt.data.key in circles
+  if (evt.data.key === "circle1" && evt.data.newValue) {
+    let activity = JSON.parse(evt.data.newValue);
+    console.log(`Setting Activity 1: ${activity}`);
+    // Get the index of the key in the circles list
+    //circles.forEach((item, index) => {
+    //  if evt.data.key === ""
+    //});
+    // Replace activities[index] with newvalue
+
   }
 };
 
@@ -38,4 +73,12 @@ function clockCallback(data) {
 }
 simpleClock.initialize("minutes", "longDate", clockCallback);
 
-/* --------- PROGRESS CIRCLES ---------- */
+/* --------- ACTIVITY ---------- */
+function activityCallback(data) {
+  activities.forEach((item, index) => {
+    let img = item.firstChild;
+    let txt = img.nextSibling;
+    txt.text = data[Object.keys(data)[index]].pretty;
+  });
+}
+activityArcs.initialize("seconds", activityCallback);
--- settings/index.jsx
+++ settings/index.jsx
@@ -15,22 +15,44 @@ function mySettings(props) {
           ]}
         />
       </Section>
+
       <Section
-        title={<Text bold align="center">Date and Time</Text>}>
-        <Toggle
-          settingsKey="toggle"
-          label="Toggle Switch"
+        title={<Text bold align="center">Activity 1</Text>}>
+        <Select
+          label={'multiselection'}
+          settingsKey="circle1"
+            options={[
+              {name:"steps",          value:"1"},
+              {name:"Distance",       value:"2"},
+              {name:"Calories",       value:"3"},
+              {name:"Active Minutes", value:"4"}
+            ]}
         />
-        <ColorSelect
-          settingsKey="color"
-          colors={[
-            {color: "tomato"},
-            {color: "sandybrown"},
-            {color: "#FFD700"},
-            {color: "#ADFF2F"},
-            {color: "deepskyblue"},
-            {color: "plum"}
-          ]}
+      </Section>
+      <Section
+        title={<Text bold align="center">Activity 2</Text>}>
+        <Select
+          label={'multiselection'}
+          settingsKey="circle2"
+            options={[
+              {name:"steps",          value:"1"},
+              {name:"Distance",       value:"2"},
+              {name:"Calories",       value:"3"},
+              {name:"Active Minutes", value:"4"}
+            ]}
+        />
+      </Section>
+      <Section
+        title={<Text bold align="center">Activity 3</Text>}>
+        <Select
+          label={'multiselection'}
+          settingsKey="circle3"
+            options={[
+              {name:"steps",          value:"1"},
+              {name:"Distance",       value:"2"},
+              {name:"Calories",       value:"3"},
+              {name:"Active Minutes", value:"4"}
+            ]}
         />
       </Section>
     </Page>
